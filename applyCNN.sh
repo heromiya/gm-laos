fd6caf1c455ff65564f1c96dabbf39e5068fab44
heromiya <heromiya@heromiya.net>
2015-08-20 02:20:57 +0900

    modified:   applyCNN.sh

--- applyCNN.sh
+++ applyCNN.sh
@@ -26,15 +26,22 @@ for ARGS in `iojs ../../../get.BingAerial.js $LONMIN $LATMIN $LONMAX $LATMAX $ZL
 	echo a${QKEY}.tif >> tmp.txt
     fi
 done
-gdalbuildvrt -overwrite -input_file_list tmp.txt tmp.vrt
-gdal_translate -co compress=deflate tmp.vrt tmp_bing.tif
+gdalbuildvrt -overwrite -input_file_list tmp.txt tmp_bing.vrt
+#gdal_translate -co compress=deflate tmp.vrt tmp_bing.tif
 
-XMIN=`gdalinfo tmp_bing.tif | grep "Upper Left" | sed 's/^Upper Left  (\([-.0-9]*\), \([-.0-9]*\)) .*/\1/g'`
-YMIN=`gdalinfo tmp_bing.tif | grep "Lower Right"| sed 's/^Lower Right (\([-.0-9]*\), \([-.0-9]*\)) .*/\2/g'`
-XMAX=`gdalinfo tmp_bing.tif | grep "Lower Right"| sed 's/^Lower Right (\([-.0-9]*\), \([-.0-9]*\)) .*/\1/g'`
-YMAX=`gdalinfo tmp_bing.tif | grep "Upper Left" | sed 's/^Upper Left  (\([-.0-9]*\), \([-.0-9]*\)) .*/\2/g'`
-XRES=`gdalinfo tmp_bing.tif | grep "Pixel Size" | sed 's/Pixel Size = (\([-.0-9]*\),\([-.0-9]*\))/\1/'`
-YRES=`gdalinfo tmp_bing.tif | grep "Pixel Size" | sed 's/Pixel Size = (\([-.0-9]*\),\([-.0-9]*\))/\2/'`
+XMIN=`gdalinfo tmp_bing.vrt | grep "Upper Left" | sed 's/^Upper Left  (\([-.0-9]*\), \([-.0-9]*\)) .*/\1/g'`
+YMIN=`gdalinfo tmp_bing.vrt | grep "Lower Right"| sed 's/^Lower Right (\([-.0-9]*\), \([-.0-9]*\)) .*/\2/g'`
+XMAX=`gdalinfo tmp_bing.vrt | grep "Lower Right"| sed 's/^Lower Right (\([-.0-9]*\), \([-.0-9]*\)) .*/\1/g'`
+YMAX=`gdalinfo tmp_bing.vrt | grep "Upper Left" | sed 's/^Upper Left  (\([-.0-9]*\), \([-.0-9]*\)) .*/\2/g'`
+XRES=`gdalinfo tmp_bing.vrt | grep "Pixel Size" | sed 's/Pixel Size = (\([-.0-9]*\),\([-.0-9]*\))/\1/'`
+YRES=`gdalinfo tmp_bing.vrt | grep "Pixel Size" | sed 's/Pixel Size = (\([-.0-9]*\),\([-.0-9]*\))/\2/'`
+
+g.region n=$YMAX s=$YMIN e=$XMAX w=$XMIN nsres=$YRES ewres=$XRES --overwrite
+r.external -o input=tmp.vrt output=bing --overwrite
+v.in.ogr   -o dsn=gt/merge.shp output=gt type=boundary --overwrite
+
+
+
 
 gdalwarp -overwrite -te $XMIN $YMIN $XMAX $YMAX -tr $XRES $YRES -wm 1024 -multi -co compress=Deflate $HOME/grene-mg-01/JRC-GHS/MT.vrt tmp_GHS.tif
 
